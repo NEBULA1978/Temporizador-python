b34c2cd78f6803f8edf3b79e0ff414cc5be5faf6
NextCodigos <nebulanemesis376@gmail.com>
2022-03-25 17:34:12 +0100

    subiendo los primeros archivo

---
+++ temporizador.py
@@ -0,0 +1,8 @@
+import time
+segundos=int(input("Digite segundos desados: "))
+for i in range(segundos):
+    print(str(segundos -i) + " segundos retantes")
+    time.sleep(1)
+print("Booommm Exploto¡¡¡")
+
+#despues del tiempo  ejecuta accion
